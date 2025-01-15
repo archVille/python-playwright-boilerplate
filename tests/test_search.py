3ac9e7b81fb70475a3e3ee7c877e20a6a52f0c9d
panagiotis.meretis <pmeretis@gmail.com>
2025-01-15 13:05:51 +0000

    add report to ignore for allure-report folder and comment out tests accordingly

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,12 +1,12 @@
 import pytest 
 
-# @pytest.mark.only_browser("chromium")
-# def test_h1_title(page):
-#     page.goto('/')
-#     assert page.inner_text('h1') == 'Όλες οι Ειδήσεις'
+@pytest.mark.only_browser("chromium")
+def test_h1_title(page):
+    page.goto('/')
+    assert page.inner_text('h1') == 'Όλες οι Ειδήσεις'
     
 @pytest.mark.only_browser("chromium")
 def test_Start_Confirm(page):
      page.goto('/')
-     page.click('[aria-label="Συναίνεση"]')
+    #  page.click('[aria-label="Συναίνεση"]')
      
